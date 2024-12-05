8040d39a7ea8fefc558c5cb1f08be0eb8a3bc8bf
HimanshuShipease <himanshu.grover@shipease.in>
2024-12-05 12:32:26 +0530

    Footer UI Updated

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-dark text-white py-3">
+      <div className="container text-center">
+        <p>&copy; 2024 My Website. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
