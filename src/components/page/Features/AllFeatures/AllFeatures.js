6a347e501c370688587cf28e715479f865f71484
HimanshuShipease <himanshu.grover@shipease.in>
2024-12-14 13:10:03 +0530

    All Features Page UI Updated

---
+++ src/components/page/Features/AllFeatures/AllFeatures.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './AllFeatures.css';
+import features from "./features.json"
+
+const AllFeatures = () => {
+  return (
+    <div className="all-features-section">
+      <h2 className="features-heading">All Features</h2>
+      <div className="features-list">
+        {features.map((feature, index) => (
+          <div className="feature-card" key={index}>
+            <div className="card">
+              <div className="card__content">
+                <h3 className="card__title">{feature.title}</h3>
+                <ul className='benefit-list'>
+                  {feature.benefits.map((benefit, i) => (
+                    <li key={i} className="benefit-item">
+                      {/* ✅ */} {benefit}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="card-front">
+                <div className="feature-icon">
+                  <span>{feature.icon}</span>
+                </div>
+                <h3 className="feature-title">{feature.title}</h3>
+                <p className="card__description">{feature.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllFeatures;
+
+
+
