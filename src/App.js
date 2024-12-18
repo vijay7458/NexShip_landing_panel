c348f3fd3b3b4d2ecce0999aa3d06f11d02d3718
HimanshuShipease <himanshu.grover@shipease.in>
2024-12-18 12:05:23 +0530

    Hyper Local Page UI Updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import RateCalculator from './components/page/Offerings/RateCalculator/RateCalcu
 import CarrierIntegration from './components/page/Integration/CarrierIntegration/CarrierIntegration';
 import ReferAndEarn from './components/page/Resources/ReferAndEarn/ReferAndEarn';
 import ContactUs from './components/page/ContactUs/ContactUs';
+import HyperLocal from './components/page/Features/HyperLocal/HyperLocal';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path='/damaged-shipment' element={<DamagedShipment />} />
           <Route path='/shipment-protection' element={<ShipmentProtection />} />
           <Route path='/ndr-management' element={<NDRManagement />} />
+          <Route path='/hyper-local' element={<HyperLocal />} />
           <Route path='/rate-calculator' element={<RateCalculator />} />
           <Route path='/carrier-integration' element={<CarrierIntegration />} />
           <Route path='/refer-and-earn' element={<ReferAndEarn />} />
