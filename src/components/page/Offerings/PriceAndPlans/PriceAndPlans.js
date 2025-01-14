040ba6acc0989190bd5dbce08eb6036a63419af9
HimanshuShipease <himanshu.grover@shipease.in>
2025-01-14 18:53:02 +0530

    Plan And Price Page UI Updated

--- src/components/page/Offerings/PriceAndPlans/PriceAndPlans.js
+++ src/components/page/Offerings/PriceAndPlans/PriceAndPlans.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import './PriceAndPlans.css'
 import ListIcon from './ListIcon'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { Navigate, useNavigate } from 'react-router';
 
 const PriceAndPlans = () => {
+    let Navigate = useNavigate();
+
+    const handleScroll = () => {
+        window.scrollTo({
+            top: 550,
+            behavior: "smooth",
+        });
+    };
     return (
         <>
             <section className='business-plan-page'>
+                <header className="page-header">
+                    <div className="ripple-shape">
+                        <span className="ripple-1" />
+                        <span className="ripple-2" />
+                        <span className="ripple-3" />
+                        <span className="ripple-4" />
+                        <span className="ripple-5" />
+                    </div>
+                    <h1 className=''>Find the Right Plan to Power Your Logistics</h1>
+                    <p className="page-header-description">
+                        Affordable, Transparent, and Tailored Pricing to Meet Your Business Goals
+                    </p>
+                    <button onClick={handleScroll} className='btn main-button'>Explore More <FontAwesomeIcon className='ms-2' icon={faArrowDown} /></button>
+                </header>
                 <h2 className='mt-3 text-center'>Grab The Perfect Plan for Your Needs</h2>
                 <p className='text-center text-sh-primary'>Our transparent pricing makes it easy to find a plan that works within your financial constraints.</p>
                 <div className='plans-container'>
@@ -64,7 +89,7 @@ const PriceAndPlans = () => {
                 </div>
                 <div className='book-demo-sec'>
                     <p className='mb-0'>Grab It Fast to Get Special Price!</p>
-                    <button className='btn'>Book Demo Now!</button>
+                    <button onClick={() => Navigate("/contact")} className='btn'>Book Demo Now!</button>
                 </div>
             </section>
         </>
