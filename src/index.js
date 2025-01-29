9437adc43da54e72bdcbc98ed2987b7bdbcccdfd
HimanshuShipease <himanshu.grover@shipease.in>
2025-01-29 19:02:07 +0530

    Global Styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { router } from './routes';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
