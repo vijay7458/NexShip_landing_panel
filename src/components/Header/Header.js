162eec56eefbc6f47c468ee83b945aae77131e19
HimanshuShipease <himanshu.grover@shipease.in>
2025-02-24 19:48:26 +0530

    Header Responsive UI Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/image/logo.svg";
-import LogoAnimation from "./Effects/LogoAnimation/LogoAnimation";
-import AllFeaturesIcon from "./NavIcons/AllFeaturesIcon";
+// import LogoAnimation from "./Effects/LogoAnimation/LogoAnimation";
+// import AllFeaturesIcon from "./NavIcons/AllFeaturesIcon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarsProgress, faBoxOpen, faCalculator, faCloud, faHandHoldingDollar, faHandsHoldingCircle, faIndianRupeeSign, faLocationDot, faMapLocation, faMoneyBill1, faRightToBracket, faRoadBarrier, faShuffle, faSuitcase, faUserPlus, faUsers, faWallet } from "@fortawesome/free-solid-svg-icons";
 import { faAmazon, faMedium, faSquareWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import sampleVideo from '../../assets/image/sample.webm'
-import mobileLogo from "../../assets/image/mobileLogo.svg";
+// import sampleVideo from '../../assets/image/sample.webm'
+// import mobileLogo from "../../assets/image/mobileLogo.svg";
 import TrackingIcon from "./NavIcons/TrackingIcon";
 import { Link } from "react-router-dom";
 
@@ -89,12 +89,12 @@ const Header = () => {
                 Features
               </span>
               <ul className="dropdown-menu row">
-                <li className="col-3">
+                <li className="col-12 col-lg-3">
                   <h4 className="mb-2">We understand the art of scaling rapidly</h4>
                   <p>Founded in 2020 by Ajay, Lalit, and Pawan, ShipEase has quickly emerged as one of India’s fastest-growing SaaS companies.</p>
                   <button className="btn">Explore More</button>
                 </li>
-                <li className="col-9">
+                <li className="col-12 col-lg-9">
                   <ul>
                     <li><FontAwesomeIcon icon={faSuitcase} /><a className="dropdown-item" href="/all-features">All Features<hr /><span>Explore Our Full Feature Set</span></a></li>
                     <li><FontAwesomeIcon icon={faBoxOpen} /><a className="dropdown-item" href="/damaged-shipment">Damaged Shipment<hr /><span>Advanced Recommendation Engine</span></a></li>
@@ -104,7 +104,7 @@ const Header = () => {
                     <li><FontAwesomeIcon icon={faAmazon} /><a className="dropdown-item" href="/amazon-self-ship">Amazon Self Ship<hr /><span>Seamless Amazon Self-Shipping with Shipease</span></a></li>
                     <li><FontAwesomeIcon icon={faMoneyBill1} /><a className="dropdown-item" href="/early-cod">Early COD<hr /><span>Enjoy Daily COD Remittance</span></a></li>
                     <li><FontAwesomeIcon icon={faLocationDot} /><a className="dropdown-item" href="/pickup-locations">Pickup Locations<hr /><span>Optimize Your Pickup Strategy</span></a></li>
-                    <li><FontAwesomeIcon icon={faUserPlus} /><a className="dropdown-item signup-box" target="_blank" href="https://app.shipease.in/sign-up">Create Account<hr /><span>Sign Up with Shipease Today</span></a></li>
+                    <li><FontAwesomeIcon icon={faUserPlus} /><a className="dropdown-item signup-box" target="_blank" rel="noreferrer" href="https://app.shipease.in/sign-up">Create Account<hr /><span>Sign Up with Shipease Today</span></a></li>
                   </ul>
                 </li>
               </ul>
@@ -193,7 +193,7 @@ const Header = () => {
           <button onClick={handleNavigation} className="btn login-button" title="Login">
             Login
           </button>
-          <button onClick={handleToggle} id="nav-icon3" className={`btn ${ToggleOpen && "open"}`}>
+          <button onClick={handleToggle} id="toggle-icon" className={`btn ${ToggleOpen && "open"}`}>
             <span></span>
             <span></span>
             <span></span>
