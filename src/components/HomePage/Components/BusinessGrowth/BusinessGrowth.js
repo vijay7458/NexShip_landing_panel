73a32e4a28313f400c920ce70976d607010338fb
HimanshuShipease <himanshu.grover@shipease.in>
2025-03-26 15:36:17 +0530

    Business Growth section UI Updated

--- src/components/HomePage/Components/BusinessGrowth/BusinessGrowth.js
+++ src/components/HomePage/Components/BusinessGrowth/BusinessGrowth.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './BusinessGrowth.css';
-import DashboardImage from '../../../../assets/image/dashboardImage.png'
+import DashboardImage from '../../../../assets/image/dashboardImage1.png'
 // import IncreaseBusiness from '../../../../assets/image/IncreaseBusiness.png'
 // import EnhanceMarketing from '../../../../assets/image/EnhanceMarketing.png'
 import ExpandReach from './ExpandReach';
@@ -25,37 +25,43 @@ const BusinessGrowth = () => {
               className="dashboard-image"
             />
           </div>
-          <div className="features-grid">
-            <div className="feature-item">
-              <div className='business-icon'>
-                <IncreaseBusiness />
+
+        </div>
+        <div className="bg-fetaure-section">
+          <div className='overview-container'>
+            <div className="features-grid">
+              <div className="feature-item">
+                <div className='business-icon'>
+                  <IncreaseBusiness />
+                </div>
+                <h3 className="bg-feature-title">Increase Your Business</h3>
+                <p className="feature-description">
+                  Boost your revenue with smarter tools and actionable insights tailored to your goals.
+                </p>
               </div>
-              <h3 className="feature-title">Increase Your Business</h3>
-              <p className="feature-description">
-                Boost your revenue with smarter tools and actionable insights tailored to your goals.
-              </p>
-            </div>
-            <div className="feature-item">
-              <div className='business-icon'>
-                <EnhanceMarketing />
+              <div className="feature-item">
+                <div className='business-icon'>
+                  <EnhanceMarketing />
+                </div>
+                <h3 className="bg-feature-title">Enhance Your Marketing</h3>
+                <p className="feature-description">
+                  Promote your brand effectively with targeted strategies and creative campaigns.
+                </p>
               </div>
-              <h3 className="feature-title">Enhance Your Marketing</h3>
-              <p className="feature-description">
-                Promote your brand effectively with targeted strategies and creative campaigns.
-              </p>
-            </div>
-            <div className="feature-item">
-              <div className='business-icon'>
-                <ExpandReach />
+              <div className="feature-item">
+                <div className='business-icon'>
+                  <ExpandReach />
+                </div>
+                <h3 className="bg-feature-title">Expand Your Reach</h3>
+                <p className="feature-description">
+                  Scale your business across multiple platforms and unlock new opportunities.
+                </p>
               </div>
-              <h3 className="feature-title">Expand Your Reach</h3>
-              <p className="feature-description">
-                Scale your business across multiple platforms and unlock new opportunities.
-              </p>
             </div>
           </div>
         </div>
       </div>
+
 
     </>
   );
