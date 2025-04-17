82c95671ab8d2a69cb9b624302602d72b2c63a4b
HimanshuShipease <himanshu.grover@shipease.in>
2025-04-17 12:16:10 +0530

    Media page UI Updated and articles added

--- src/components/page/Resources/Media/Media.js
+++ src/components/page/Resources/Media/Media.js
@@ -2,76 +2,21 @@ import React from 'react';
 import './Media.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import MediaContent from './MediaContent';
 
 const Media = () => {
-    const mediaContent = [
-        {
-            date: "June 19, 2023",
-            title: "ShipEase Technologies Pvt Ltd: Leveraging Technology to Enhance Your Shipping Experience",
-            description: "ShipEase Technologies is revolutionizing logistics with cutting-edge technology, optimizing every aspect of the shipping journey.",
-            author: "Startup Story™",
-        },
-        {
-            date: "June 19, 2023",
-            title: "Not All Funding Announcements Are Equal",
-            description: "We’re excited to share that we've received validation from a major financial institution, a huge step forward for our growth.",
-            author: "Ajay K",
-        },
-        {
-            date: "June 19, 2023",
-            title: "Logistics Tech: ShipEase Technologies",
-            description: "With Recur Club’s non-dilutive funding, we now have the capital to continue innovating and advancing our solutions.",
-            author: "Recur Club",
-        },
-        {
-            date: "June 19, 2023",
-            title: "ShipEase Technologies Pvt Ltd: Tech for the Future",
-            description: "Success in business isn’t about offering discounts—it’s about solving real-world problems with impactful solutions.",
-            author: "Startup Story™",
-        },
-        {
-            date: "June 19, 2023",
-            title: "The Supply Chain Observer: Industry News & Insights",
-            description: "The Supply Chain Observer offers the latest updates and in-depth articles on the global supply chain and logistics sectors.",
-            author: "Pranav Jyoti",
-        },
-        {
-            date: "June 19, 2023",
-            title: "Shipping Your Products Made Easy",
-            description: "Wondering how you’ll ship your products? With ShipEase, your goods are in trusted hands, ensuring safe and timely delivery.",
-            author: "ShipEase",
-        },
-        {
-            date: "July 09, 2023",
-            title: "Exciting Partnership Announcement",
-            description: "We're thrilled to announce our exclusive partnership, helping D2C brands manage checkout and shipping with ease.",
-            author: "Lalit Singh",
-        },
-        {
-            date: "June 19, 2023",
-            title: "Effortless Cash on Delivery Shipments",
-            description: "ShipEase makes cash on delivery orders hassle-free, offering reliable shipping solutions for a seamless experience.",
-            author: "ShipEase",
-        },
-        {
-            date: "June 20, 2023",
-            title: "Exclusive Interview: Pawan Kumar, Co-founder of ShipEase",
-            description: "In an exclusive chat with MediaBrief, Pawan Kumar, Co-founder of ShipEase, discusses the company’s mission and vision for the future of logistics.",
-            author: "Mediabrief.com",
-        },
-        {
-            date: "July 09, 2023",
-            title: "Reliable Shipping Solutions with ShipEase",
-            description: "Trust ShipEase to handle your shipments with care and precision, wherever your business takes you.",
-            author: "ShipEase Technology",
-        },
-    ];
+
+
     const handleScroll = () => {
         window.scrollTo({
             top: 550,
             behavior: "smooth",
         });
     };
+
+    const handleOpenLink = (url) => {
+        window.open(url, '_blank')
+    }
 
     return (
         <>
@@ -88,15 +33,22 @@ const Media = () => {
                 <button onClick={handleScroll} className='btn main-button'>Explore More <FontAwesomeIcon className='ms-2' icon={faArrowDown} /></button>
             </header>
             <div className="media-page">
-                <h1 className="media-heading">Media Highlights</h1>
+                <h2 className="heading text-center pt-5">Media Highlights</h2>
                 <p className="media-intro">Stay updated with the latest news and stories about ShipEase.</p>
-                <div className="media-grid">
-                    {mediaContent.map((item, index) => (
-                        <div className="media-card" key={index}>
-                            <div className="media-date">{item.date}</div>
-                            <h2 className="media-title">{item.title}</h2>
-                            <p className="media-description">{item.description}</p>
-                            <div className="media-author">By {item.author}</div>
+                <div className="media-grid mb-5">
+                    {MediaContent.map((item, index) => (
+                        <div onClick={() => handleOpenLink(item?.url)} className="print-media-card" key={index}>
+                            <div className="image-container">
+                                <img src={item?.image} alt="" />
+                            </div>
+                            <div>
+                                <div className="media-date">{item.date}</div>
+                                <h2 className="media-title">{item.title}</h2>
+                                <p className="media-description">{item.description}</p>
+                            </div>
+                            <div className='w-100 text-right'>
+                                <div className="media-author">By {item.author}</div>
+                            </div>
                         </div>
                     ))}
                 </div>
---
+++ src/components/page/Resources/Media/MediaContent.js
@@ -0,0 +1,128 @@
+import SundayGuardian from "../../../../assets/image/media/SundayGuardian.png"
+import LogisticsOutlook from "../../../../assets/image/media/LogisticsOutlook.png"
+import TheFreePressJournal from "../../../../assets/image/media/TheFreePressJournal.png"
+
+const MediaContent = [
+    {
+        date: "August 05, 2023",
+        title: "Embracing 4PL: Sellers shifting for hassle-free supply chain solutions",
+        description: "Understanding, Advantages and The Future of 4PL Logistics - Opinion Piece by Pawan Kumar, Co-founder of Shipease Technologies Pvt Ltd.",
+        author: "Pawan Kumar",
+        image: SundayGuardian,
+        url: "https://sundayguardianlive.com/business/embracing-4pl-sellers-shifting-for-hassle-free-supply-chain-solutions"
+    },
+    {
+        date: "September 12, 2023",
+        title: "Decoding customer buying behaviour at regional level in India",
+        description: "An article by Pawan Kumar, Co-Founder and CEO, ShipEase. on the changing landscape of customer buying behaviour in India.",
+        author: "ITP Editor",
+        image: LogisticsOutlook,
+        url: "https://www.logisticsoutlook.com/people/decoding-customer-buying-behaviour-at-regional-level-in-india#:~:text=India%20experiences%20diverse%20climatic%20conditions,to%20seasonal%20demands%20is%20crucial."
+    },
+    {
+        date: "August 05, 2023",
+        title: "The Growing Role Of 4pl Logistics In supply Chain Management",
+        description: "Understanding, Advantages and The Future of 4PL Logistics - Opinion Piece by Pawan Kumar, Co-founder of Shipease Technologies Pvt Ltd.",
+        author: "The Sunday Guardian™",
+        image: SundayGuardian,
+        url: "/Sunday Guardian  (The Business Guardian - Biz Buzz) - 30th July - 5th August, 2023 - Pg. 20.pdf"
+    },
+    {
+        date: "August 20, 2023",
+        title: "Enhancing security in supply chains through blockchain",
+        description: "Decentralised nature of blockchain ensures enhanced security against cyber threats and fraud.",
+        author: "Pawan Kumar",
+        image: "https://www.financialexpress.com/wp-content/themes/ie-network-theme/assets/src/img/logo/fe-transformx.svg",
+        url: "https://www.financialexpress.com/business/digital-transformation-enhancing-security-in-supply-chains-through-blockchain-3224263/"
+    },
+    {
+        date: "September 8, 2023",
+        title: "Optimising Supply Chain Efficiency : How Technology is Revolutionising Logistics",
+        description: "In today’s fast-paced and interconnected world, the logistics and supply chain industry play a pivotal role in global commerce.",
+        author: "Pawan Kumar",
+        image: "https://timestech.in/wp-content/uploads/2018/10/timestech-logo.png",
+        url: "https://timestech.in/optimising-supply-chain-efficiency-how-technology-is-revolutionising-logistics/"
+    },
+    {
+        date: "September 29, 2023",
+        title: "Reverse Logistics in the Digital Age: Managing Returns with Tech-Driven Solutions in India",
+        description: "In today’s fast-paced digital economy, the management of reverse logistics has become a critical aspect of supply chain operations.",
+        author: "Pawan Kumar",
+        image: "https://onlinemediacafe.com/wp-content/uploads/2021/05/cropped-logo-new.png",
+        url: "https://onlinemediacafe.com/business/reverse-logistics-in-the-digital-age-managing-returns-with-tech-driven-solutions-in-india/"
+    },
+    {
+        date: "September 18, 2023",
+        title: "From Inception to Innovation: Know About ShipEase Revolutionizing Logistics Through SaaS and Technology",
+        description: "The platform leverages a spectrum of technologies, including artificial intelligence (AI), machine learning (ML), and big data analytics.",
+        author: "Oliviya Kunjumon",
+        image: TheFreePressJournal,
+        url: "https://www.freepressjournal.in/business/from-inception-to-innovation-know-about-shipease-revolutionizing-logistics-through-saas-and-technology"
+    },
+    {
+        date: "August 09, 2023",
+        title: "[Exclusive Interview] This SaaS-Based Logistics Company Is Leveraging AI & Other Technologies To Ensure Stunning Performance",
+        description: "Recently, we interacted with Mr. Pawan Kumar, co-founder & CEO of ShipEase – one of the fastest-growing SaaS-enabled logistics company in India.",
+        author: "Mohul Ghosh",
+        image: "https://trak.in/stories//wp-content/uploads/2022/10/Trak.in-final-logo.svg",
+        url: "https://trak.in/stories/exclusive-interview-this-saas-based-logistics-company-is-leveraging-ai-other-technologies-to-ensure-stunning-performance/"
+    },
+    {
+        date: "August 21, 2023",
+        title: "Global innovators celebrate World Entrepreneurs' Day: Igniting ideas, empowering change!",
+        description: "The day acknowledges the role they play in shaping the business landscape.",
+        author: "Rohin Ramesh",
+        image: "https://www.indiantelevision.com/themes/custom/gavias_batiz/logo.png",
+        url: "https://www.indiantelevision.com/mam/marketing/mam/global-innovators-celebrate-world-entrepreneurs-day-igniting-ideas-empowering-change-230821"
+    },
+    {
+        date: "August 21, 2023",
+        title: "Entrepreneurs, CEOs, Industry Leaders React On World Entrepreneur's Day - 2023",
+        description: "August 21st, 2023 is celebrated as World Entrepreneur’s Day to appreciate and acknowledge all risk-takers and dare-devils, who are determined to create their own legacy by launching and running their startups and businesses.",
+        author: "Mohul Ghosh",
+        image: "https://trak.in/stories//wp-content/uploads/2022/10/Trak.in-final-logo.svg",
+        url: "https://trak.in/stories/entrepreneurs-ceos-industry-leaders-react-on-world-entrepreneurs-day-2023-check-top-quotes-views-more/"
+    },
+    {
+        date: "August 21, 2023",
+        title: "Global Trailblazers: Celebrating the Spirit of World Entrepreneurs Day",
+        description: "World Entrepreneurs Day is a celebration of the visionary individuals who dare to dream, innovate, and create.",
+        author: "CXOtoday News Desk",
+        image: "https://cxotoday.com/wp-content/uploads/2018/08/cxo-logo.png",
+        url: "https://cxotoday.com/cxo-bytes/global-trailblazers-celebrating-the-spirit-of-world-entrepreneurs-day/"
+    },
+    {
+        date: "July 13, 2023",
+        title: "Revolutionising the supply chain: Artificial intelligence’s implications in logistics",
+        description: "The logistics industry, with its intricate transportation, warehousing, and distribution network, serves as the backbone of global commerce.",
+        author: "Pawan Kumar",
+        image: "https://www.constructionweekonline.in/cloud/2021/11/23/cwo-logo.png",
+        url: "https://www.constructionweekonline.in/uncategorized/revolutionising-the-supply-chain-artificial-intelligences-implications-in-logistics"
+    },
+    {
+        date: "July 10, 2023",
+        title: "Revolutionizing the supply chain: Unveiling the implications of AI in the logistics industry",
+        description: "The logistics industry, with its intricate network of transportation, warehousing, and distribution, serves as the backbone of global commerce.",
+        author: "Pawan Kumar",
+        image: "https://cxotoday.com/wp-content/uploads/2018/08/cxo-logo.png",
+        url: "https://cxotoday.com/cxo-bytes/revolutionizing-the-supply-chain-unveiling-the-implications-of-ai-in-the-logistics-industry/"
+    },
+    {
+        date: "July 10, 2023",
+        title: "AI in Logistics: How AI is Revolutionizing the Supply Chain",
+        description: "The logistics industry, with its intricate network of transportation, warehousing, and distribution, serves as the backbone of global commerce.",
+        author: "Pawan Kumar",
+        image: "https://timestech.in/wp-content/uploads/2018/10/timestech-logo.png",
+        url: "https://timestech.in/ai-in-logistics-how-ai-is-revolutionizing-the-supply-chain/"
+    },
+    {
+        date: "July 10, 2023",
+        title: "ShipEase: The Journey of Reinventing Logistics through User-Friendly SaaS Technology and Innovative Solutions",
+        description: "The logistics industry, with its intricate network of transportation, warehousing, and distribution, serves as the backbone of global commerce.",
+        author: "Pawan Kumar",
+        image: "https://cxotoday.com/wp-content/uploads/2018/08/cxo-logo.png",
+        url: "https://cxotoday.com/interviews/shipease-the-journey-of-reinventing-logistics-through-user-friendly-saas-technology-and-innovative-solutions/"
+    }
+]
+
+export default MediaContent;
